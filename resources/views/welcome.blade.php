c4364fc2b35cd9969298629133d3f9ec707d003a
Emrah Emrem <emrahemremm@gmail.com>
2018-03-02 16:46:21 +0300

    asdasd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Zu spat
+                    Blood Friend
                 </div>
 
                 <div class="links">

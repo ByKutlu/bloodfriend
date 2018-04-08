27c386f9fc159ea0995f3d18432d3a2c2e9a7279
Osman Kutlu <by.43.kutlu@gmail.com>
2018-04-08 17:03:30 +0300

    Merge branch 'master' of https://github.com/ByKutlu/bloodfriend
    # Conflicts:
    #	resources/views/calisan.blade.php
    #	resources/views/kurum.blade.php
    #	resources/views/main_template.blade.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -36,6 +37,38 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        $request->session()->put('user_id', $user->user_id);
+        $request->session()->put('email', $user->email);
+        $request->session()->put('name', $user->name);
+        $request->session()->put('surname', $user->surname);
+        $request->session()->put('town_id', $user->town_id);
+        $yetki = "donor";
+        $userType = "donor";
+        $userTypes = DB::table('user_type')->select('type')->where('user_id',$user->user_id)->get();
+        $count = 0;
+        foreach($userTypes as $userTypee){
+            if($userTypee->type == "employee"){
+                $yetki="employee";
+                $userType="employee";
+                $request->session()->put('employee_role', DB::table('employee')->select('role')->where('user_id',$user->user_id)->get()[0]->role);
+                $count++;
+            }
+            if($userTypee->type == "admin"){
+                $yetki="admin";
+                $count++;
+            }
+        }
+        if($count>=2){
+            $yetki="pro";
+        }
+        $request->session()->put('yetki', $yetki);
+        $request->session()->put('user_type', $userType);
+        //$request->session()->put('surname', $user->roles());
+        //
     }
 
 }
